2865fbc2ef1daeaa919e4b82835220894a0e5999
John Bugner <john.bugner@gmail.com>
2016-09-26 08:04:24 -0500

    Changed tabs to spaces.

--- src/Native/Vibration.js
+++ src/Native/Vibration.js
@@ -3,15 +3,15 @@
 //import Native.Utils //
 
 var _JohnBugner$elm_vibration$Native_Vibration = function() {
-	function vibrateInPattern(pattern) {
-		return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
-			window.navigator.vibrate(_elm_lang$core$Native_List.toArray(pattern));
+    function vibrateInPattern(pattern) {
+        return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+            window.navigator.vibrate(_elm_lang$core$Native_List.toArray(pattern));
 
-			callback(_elm_lang$core$Native_Utils.Tuple0);
-		});
-	}
+            callback(_elm_lang$core$Native_Utils.Tuple0);
+        });
+    }
 
-	return {
-		vibrateInPattern : vibrateInPattern,
-	};
+    return {
+        vibrateInPattern : vibrateInPattern,
+    };
 }();
